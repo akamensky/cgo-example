581fb24de2c2981057d73d45dba4ed7052724abc
Alexey Kamenskiy <me@akamensky.com>
2016-05-01 00:40:26 +0800

    Fix build.bat to build test executable

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 set GOPATH=%cd%
-go build -buildmode=c-archive foo
-gcc -m64 -mthreads -fmessage-length=0 -Wl,-no_pie -I pkg\windows_amd64 -o test.exe bar.c pkg\windows_amd64\foo.a
+go install -buildmode=c-archive foo
+gcc -m64 -mthreads -fmessage-length=0 -I pkg\windows_amd64 -o test.exe bar.c pkg\windows_amd64\foo.a -l ntdll -l winmm -l Ws2_32
