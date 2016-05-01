f4221e2e850a0ac9778915953bf4192165d8a1a1
Alexey Kamenskiy <me@akamensky.com>
2016-05-01 13:53:43 +0800

    Some compilation optimizations

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 set GOPATH=%cd%
 go install -buildmode=c-archive foo
-gcc -m64 -mthreads -fmessage-length=0 -I pkg\windows_amd64 -o test.exe bar.c pkg\windows_amd64\foo.a -l ntdll -l winmm -l Ws2_32
+gcc -g -O2 -s -m64 -mthreads -fwhole-program -fmessage-length=0 -I pkg\windows_amd64 -o test.exe bar.c pkg\windows_amd64\foo.a -l ntdll -l winmm -l Ws2_32
